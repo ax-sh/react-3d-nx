21267fbac58450360c93815a2b85310fbdb30fb0
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-12-22 23:30:35 +0545

    refactor loader

--- apps/react-3d/src/app/app.tsx
+++ apps/react-3d/src/app/app.tsx
@@ -3,9 +3,10 @@ import { Suspense } from 'react';
 import { useRoutes } from 'react-router-dom';
 
 import routes from '~react-pages';
+import { Loader } from './loader';
 
 export function App() {
-  return <Suspense fallback={<p>Loading...</p>}>{useRoutes(routes)}</Suspense>;
+  return <Suspense fallback={<Loader />}>{useRoutes(routes)}</Suspense>;
 }
 
 export default App;
