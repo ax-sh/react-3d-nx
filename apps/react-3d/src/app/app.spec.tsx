d4ec46b75fdf7cadbeba025051a0e9604df38525
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-12-22 15:27:46 +0545

    fix: tests

--- apps/react-3d/src/app/app.spec.tsx
+++ apps/react-3d/src/app/app.spec.tsx
@@ -1,15 +1,24 @@
 import { render } from '@testing-library/react';
 
 import App from './app';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 describe('App', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<App />);
+    const { baseElement } = render(
+      <Router>
+        <App />
+      </Router>
+    );
     expect(baseElement).toBeTruthy();
   });
 
   it('should have a greeting as the title', () => {
-    const { getByText } = render(<App />);
-    expect(getByText(/Welcome react-3d/gi)).toBeTruthy();
+    const { getByText } = render(
+      <Router>
+        <App />
+      </Router>
+    );
+    expect(getByText(/Home/gi)).toBeTruthy();
   });
 });
