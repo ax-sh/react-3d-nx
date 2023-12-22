b31e58b16bc8c1aa87bf9ae7ba8add66d8789806
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-12-22 16:03:49 +0545

    feat: add TYPES

--- apps/react-3d/src/pages/index.tsx
+++ apps/react-3d/src/pages/index.tsx
@@ -1,15 +1,17 @@
 import React, { PropsWithChildren, useRef, useState } from 'react';
-import { Canvas, useFrame } from '@react-three/fiber';
+import { Canvas, useFrame, MeshProps } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 
-function Box(props) {
+import { Mesh } from 'three';
+
+function Box(props: MeshProps) {
   // This reference will give us direct access to the mesh
-  const meshRef = useRef();
+  const meshRef = useRef<Mesh>(null!);
   // Set up state for the hovered and active state
   const [hovered, setHover] = useState(false);
   const [active, setActive] = useState(false);
   // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => (meshRef.current.rotation.x += delta));
+  useFrame((state, delta) => (meshRef.current!.rotation.x += delta));
   // Return view, these are regular three.js elements expressed in JSX
   return (
     <mesh
