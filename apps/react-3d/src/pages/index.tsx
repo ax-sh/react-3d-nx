3e99797c4cbffab39d22391ded95f1444981a681
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-12-22 15:40:56 +0545

    feat: add demo component

--- apps/react-3d/src/pages/index.tsx
+++ apps/react-3d/src/pages/index.tsx
@@ -1,3 +1,44 @@
+import { createRoot } from 'react-dom/client';
+import React, { PropsWithChildren, useRef, useState } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+
+function Box(props) {
+  // This reference will give us direct access to the mesh
+  const meshRef = useRef();
+  // Set up state for the hovered and active state
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((state, delta) => (meshRef.current.rotation.x += delta));
+  // Return view, these are regular three.js elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={meshRef}
+      scale={active ? 1.5 : 1}
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  );
+}
+
+function Studio({ children }: PropsWithChildren) {
+  return <Canvas className="w-full h-full">{children}</Canvas>;
+}
+
 export default function Page() {
-  return <main className="bg-indigo-500 p-2 font-mono">Home</main>;
+  return (
+    <section className="bg-black min-h-screen h-screen">
+      <Studio>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Box position={[-1.2, 0, 0]} />
+        <Box position={[1.2, 0, 0]} />
+      </Studio>
+    </section>
+  );
 }
