b50dbdbe13a9789f5acf43ef3cefc470cb86e03e
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-12-22 17:17:05 +0545

    feat: add react query mock data

--- apps/react-3d/src/app/nx-welcome.stories.tsx
+++ apps/react-3d/src/app/nx-welcome.stories.tsx
@@ -4,15 +4,31 @@ import { NxWelcome } from './nx-welcome';
 import { within } from '@storybook/testing-library';
 import { expect } from '@storybook/jest';
 
+import withAPIRequest from './with-api-request';
+import { DefaultBodyType, http, HttpResponse, PathParams } from 'msw';
+import { TanStackQueryMockData } from '../mocks/tan-stack-query-mock.data';
+
 const meta: Meta<typeof NxWelcome> = {
   component: NxWelcome,
   title: 'NxWelcome',
+  decorators: [withAPIRequest],
 };
 export default meta;
 type Story = StoryObj<typeof NxWelcome>;
 
-export const Primary = {
+export const Primary: Story = {
   args: {},
+};
+
+Primary.parameters = {
+  msw: {
+    handlers: [
+      http.get<PathParams, DefaultBodyType, typeof TanStackQueryMockData>(
+        'https://api.github.com/repos/TanStack/query',
+        () => HttpResponse.json(TanStackQueryMockData)
+      ),
+    ],
+  },
 };
 
 export const Heading: Story = {
--- apps/react-3d/src/app/nx-welcome.tsx
+++ apps/react-3d/src/app/nx-welcome.tsx
@@ -5,7 +5,29 @@
  Delete this file and get started with your project!
  * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  */
+import { useQuery } from '@tanstack/react-query';
+
 export function NxWelcome({ title }: { title: string }) {
+  const { isPending, error, data } = useQuery({
+    queryKey: ['repoData'],
+    queryFn: () =>
+      fetch('https://api.github.com/repos/TanStack/query').then((res) =>
+        res.json()
+      ),
+  });
+  if (isPending) return 'Loading...';
+
+  if (error) return 'An error has occurred: ' + error.message;
+
+  return (
+    <div>
+      <h1>{data.name}</h1>
+      <p>{data.description}</p>
+      <strong>👀 {data.subscribers_count}</strong>{' '}
+      <strong>✨ {data.stargazers_count}</strong>{' '}
+      <strong>🍴 {data.forks_count}</strong>
+    </div>
+  );
   return (
     <>
       <style
