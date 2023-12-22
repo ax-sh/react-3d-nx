2d202ab7538a691691673ac5a206de44ba7ab5a8
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-12-22 23:35:35 +0545

    add loader component

--- apps/react-3d/src/app/app.tsx
+++ apps/react-3d/src/app/app.tsx
@@ -3,7 +3,7 @@ import { Suspense } from 'react';
 import { useRoutes } from 'react-router-dom';
 
 import routes from '~react-pages';
-import { Loader } from './loader';
+import { Loader } from '@react-3d-nx/core';
 
 export function App() {
   return <Suspense fallback={<Loader />}>{useRoutes(routes)}</Suspense>;
