7524da6593fd6e1155926e1a17996bd0b74f3267
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2023-12-22 15:05:58 +0545

    feat: add vite pages

--- apps/react-3d/vite.config.ts
+++ apps/react-3d/vite.config.ts
@@ -1,4 +1,6 @@
 /// <reference types='vitest' />
+import Pages from 'vite-plugin-pages';
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
@@ -17,7 +19,7 @@ export default defineConfig({
     host: 'localhost',
   },
 
-  plugins: [react(), nxViteTsPaths()],
+  plugins: [react(), nxViteTsPaths(), Pages()],
 
   // Uncomment this if you are using workers.
   // worker: {
